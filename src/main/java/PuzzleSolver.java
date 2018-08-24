d417b38bb780eb86dca776d030e686b1726f7f58
iceburg333 <ice_burg333@yahoo.com>
2018-08-24 15:01:18 -0400

    simplest forward search

---
+++ src/main/java/PuzzleSolver.java
@@ -0,0 +1,16 @@
+public class PuzzleSolver {
+
+    public PuzzleSolver(char[][] grid) {
+    }
+
+
+    public Word find(String name) {
+        return new Word("BONES", new Point[]{
+                new Point(0, 0),
+                new Point(1, 0),
+                new Point(2, 0),
+                new Point(3, 0),
+                new Point(4, 0)
+        });
+    }
+}
---
+++ src/test/java/ForwardSearchTest.java
@@ -0,0 +1,28 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ForwardSearchTest {
+
+    @Test
+    public void simplestPossibleForwardSearch() {
+        char[][] grid = {{'B', 'O', 'N', 'E', 'S'}};
+        Point[] expectedPosition = {
+                new Point(0, 0),
+                new Point(1, 0),
+                new Point(2, 0),
+                new Point(3, 0),
+                new Point(4, 0)
+        };
+
+        PuzzleSolver solver = new PuzzleSolver(grid);
+        Word solution = solver.find("BONES");
+
+        Assert.assertEquals("BONES", solution.getName());
+
+        Assert.assertEquals(expectedPosition.length, solution.getPosition().length);
+        for (int i=0; i< expectedPosition.length; i++){
+            Assert.assertTrue(expectedPosition[i].equals(solution.getPosition()[i]));
+        }
+    }
+
+}
