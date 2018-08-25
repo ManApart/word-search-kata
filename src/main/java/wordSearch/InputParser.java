4f049354236c5755e9c1079c86894a10e5abafd5
iceburg333 <ice_burg333@yahoo.com>
2018-08-25 18:46:19 -0400

    input parser parses grid

--- src/main/java/wordSearch/InputParser.java
+++ src/main/java/wordSearch/InputParser.java
@@ -4,13 +4,25 @@ import java.util.List;
 
 public class InputParser {
     private String[] words;
+    private char[][] grid;
 
     public InputParser(List<String> input) {
         this.words = input.get(0).split(",");
 
+        grid = new char[input.size()-1][input.size()-1];
+        for (int y=1; y<input.size(); y++){
+            char[] row = input.get(y).replaceAll(",", "").toCharArray();
+            for (int x=0; x<row.length; x++){
+                grid[x][y-1] = row[x];
+            }
+        }
     }
 
     public String[] getSearchWords() {
         return words;
+    }
+
+    public char[][] getGrid() {
+        return grid;
     }
 }
--- src/test/java/wordSearch/InputParserTest.java
+++ src/test/java/wordSearch/InputParserTest.java
@@ -7,34 +7,44 @@ import java.util.Arrays;
 import java.util.List;
 
 public class InputParserTest {
-
+    List<String> input = Arrays.asList(
+            "BONES,KHAN,KIRK,SCOTTY,SPOCK,SULU,UHURA",
+            "U,M,K,H,U,L,K,I,N,V,J,O,C,W,E",
+            "L,L,S,H,K,Z,Z,W,Z,C,G,J,U,Y,G",
+            "H,S,U,P,J,P,R,J,D,H,S,B,X,T,G",
+            "B,R,J,S,O,E,Q,E,T,I,K,K,G,L,E",
+            "A,Y,O,A,G,C,I,R,D,Q,H,R,T,C,D",
+            "S,C,O,T,T,Y,K,Z,R,E,P,P,X,P,F",
+            "B,L,Q,S,L,N,E,E,E,V,U,L,F,M,Z",
+            "O,K,R,I,K,A,M,M,R,M,F,B,A,P,P",
+            "N,U,I,I,Y,H,Q,M,E,M,Q,R,Y,F,S",
+            "E,Y,Z,Y,G,K,Q,J,P,C,Q,W,Y,A,K",
+            "S,J,F,Z,M,Q,I,B,D,B,E,M,K,W,D",
+            "T,G,L,B,H,C,B,E,C,H,T,O,Y,I,K",
+            "O,J,Y,E,U,L,N,C,C,L,Y,B,Z,U,H",
+            "W,Z,M,I,S,U,K,U,R,B,I,D,U,X,S",
+            "K,Y,L,B,Q,Q,P,M,D,F,C,K,E,A,B"
+    );
 
     @Test
     public void parseSearchWords() {
-        List<String> input = Arrays.asList(
-                "BONES,KHAN,KIRK,SCOTTY,SPOCK,SULU,UHURA",
-                "U,M,K,H,U,L,K,I,N,V,J,O,C,W,E",
-                "L,L,S,H,K,Z,Z,W,Z,C,G,J,U,Y,G",
-                "H,S,U,P,J,P,R,J,D,H,S,B,X,T,G",
-                "B,R,J,S,O,E,Q,E,T,I,K,K,G,L,E",
-                "A,Y,O,A,G,C,I,R,D,Q,H,R,T,C,D",
-                "S,C,O,T,T,Y,K,Z,R,E,P,P,X,P,F",
-                "B,L,Q,S,L,N,E,E,E,V,U,L,F,M,Z",
-                "O,K,R,I,K,A,M,M,R,M,F,B,A,P,P",
-                "N,U,I,I,Y,H,Q,M,E,M,Q,R,Y,F,S",
-                "E,Y,Z,Y,G,K,Q,J,P,C,Q,W,Y,A,K",
-                "S,J,F,Z,M,Q,I,B,D,B,E,M,K,W,D",
-                "T,G,L,B,H,C,B,E,C,H,T,O,Y,I,K",
-                "O,J,Y,E,U,L,N,C,C,L,Y,B,Z,U,H",
-                "W,Z,M,I,S,U,K,U,R,B,I,D,U,X,S",
-                "K,Y,L,B,Q,Q,P,M,D,F,C,K,E,A,B"
-        );
-
         InputParser parser = new InputParser(input);
-        Assert.assertEquals(7, parser.getSearchWords().length);
-        Assert.assertEquals("BONES", parser.getSearchWords()[0]);
-        Assert.assertEquals("UHURA", parser.getSearchWords()[6]);
+        String[] searchWords = parser.getSearchWords();
+        Assert.assertEquals(7, searchWords.length);
+        Assert.assertEquals("BONES", searchWords[0]);
+        Assert.assertEquals("UHURA", searchWords[6]);
+    }
 
+    @Test
+    public void parseGrid() {
+        InputParser parser = new InputParser(input);
+        char[][] grid = parser.getGrid();
+        Assert.assertEquals(15, grid.length);
+        Assert.assertEquals(15, grid[0].length);
+        Assert.assertEquals('U', grid[0][0]);
+        Assert.assertEquals('E', grid[14][0]);
+        Assert.assertEquals('K', grid[0][14]);
+        Assert.assertEquals('B', grid[14][14]);
     }
 
 }
